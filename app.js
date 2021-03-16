e4449fc135ab54fbb1a068071276e1c60552562a
rutinsh <andispolakovs69@gmail.com>
2021-03-16 14:08:56 +0200

    Lesson 11

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const list = document.querySelector('#book-list ul');
 // delete books
 list.addEventListener('click', function(e){
     if(e.target.className == 'delete'){
-                const li = e.target.parentElement;
-                list.removeChild(li)
-        }
-    })
+        const li = e.target.parentElement;
+        list.removeChild(li)
+    }
+})
+
+//add book-list
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function(e){
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+    console.log(value);
+});
