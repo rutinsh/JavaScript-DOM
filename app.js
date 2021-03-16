0c4f2786ea0fd2c5f15fafcd75f8728a10578a79
rutinsh <andispolakovs69@gmail.com>
2021-03-16 14:27:18 +0200

    Lesson 14 and 15

--- app.js
+++ app.js
@@ -32,4 +32,16 @@ addForm.addEventListener('submit', function(e){
     li.appendChild(bookName);
     li.appendChild(deleteBtn);
     list.appendChild(li);
+
+
+});
+
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+    if(hideBox.checked){
+        list.style.display = "none";
+    } else {
+        list.style.display = "initial";
+    }
 });
