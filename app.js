6e315677a2d6888d28ee8cdaa4836b5716fe41dc
rutinsh <andispolakovs69@gmail.com>
2021-03-16 15:04:41 +0200

    Lesson 17

--- app.js
+++ app.js
@@ -58,4 +58,20 @@ searchBar.addEventListener('keyup', function(e){
             book.style.display='none';
         }
     })
+})
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', fucntion(e){
+    if(e.target.tagName =="Li"){
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        panels.forEach(function(panel){
+            if(panel ==targetPanel){
+                panel.classList.add('active');
+            } else{
+                panel.classList.remove('active');
+            }
+        })
+    }
 }) 
